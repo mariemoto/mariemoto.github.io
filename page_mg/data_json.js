b636cd2612c5a94c0c4191ef589f555be4824338
mariemoto <Marie1@Yamamoto-no-MBP.wireless.newschool.edu>
2015-05-07 20:11:11 -0400

    page_mg

---
+++ page_mg/data_json.js
@@ -0,0 +1,28 @@
+$(function() {
+      
+        function requestFile(url){
+     $.ajax(
+            {
+                url: url,
+                cache: false,
+                dataType:"json",
+                success: function(data){
+                         $("#result").html(data.status);
+                         
+                  }
+         
+             }
+     
+        );
+      }
+
+    $("#ajaxrequest").click(
+        function(){requestFile("data.json");
+            
+        }
+    )
+    
+        }
+    );
+
+
